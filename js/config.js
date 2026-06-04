adf7642b8953730cbc2bff80abfed13e84a00a11
shamil c <shamilcv61@gmail.com>
2026-06-04 22:32:44 +0530

    feat: connect website to new Supabase database instance

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@ window.CONFIG = {
   // To enable global database sync, create a free project at supabase.com,
   // create the tables using the SQL editor, and fill in the credentials below.
   // If left blank, the site will gracefully run on browser LocalStorage.
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://vptkyxbmeqghlponkdli.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_ll1Z0S0CJJE6iBxHyfD5qA_A6_d6IIu"
 };
